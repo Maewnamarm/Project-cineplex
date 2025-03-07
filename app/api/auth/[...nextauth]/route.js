4c6901ce95447518497e6fbe94799e185049ff04
Kitsada <66022387@up.ac.th>
2025-03-07 08:42:04 +0700

    แก้เทส 3

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,17 +4,14 @@ import GoogleProvider from "next-auth/providers/google";
 
 console.log("Google Client ID:", process.env.GOOGLE_CLIENT_ID);
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-};
+});
 
-const handler = NextAuth(authOptions);
-
-// ส่งออก handler ตามที่ Next.js 13+ ต้องการ
 export { handler };
 
